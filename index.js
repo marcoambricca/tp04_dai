6d061e3fc361e328c5e67d4b7a6bee4b0b60e2ce
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2024-05-02 12:10:25 -0300

    repo, service, module, dbconfig

--- index.js
+++ index.js
@@ -16,3 +16,25 @@ app.use(unknownEndpoint);
 app.listen(port, () => {
     console.log(`"server" Listening on port ${port}`);
 })
+
+app.get('/api/province', (req, res) => {
+    //Retorna array de provincias y status 200
+});
+
+app.get('/api/province', (req, res) => {
+    let provinciaId = req.query.id;
+    //Retorna status 200 y el objeto si lo encuentra, status 404 si no existe
+});
+
+app.post('/api/province', (req, res) => {
+    //Inserta una provincia, retorna status 201. status 400 y el error en caso de existir algun error en reglas de negocio
+});
+
+app.put('/api/province' , (req, res) => {
+    //Actualiza una provincia, retorna 201, 404 si no existe esa provincia y 400 si hay un error en reglas de negocio
+});
+
+app.delete('/api/province/{id}', (req, res) => {
+    let provinciaId = req.query.id;
+    //Borra una provincia y 200, 404 si no existe provincia con ese id
+});
--- src/repositories/province-repository.js
+++ src/repositories/province-repository.js
@@ -1,5 +1,7 @@
 import DBConfig from './../configs/db-config.js';
 import pkg from 'pg';
+import res from 'express/lib/response.js';
+import { rows } from 'pg/lib/defaults.js';
 const { Client, Pool } = pkg;
 
 export default class ProvinceRepository{
@@ -20,15 +22,73 @@ export default class ProvinceRepository{
     }
     
     getByIdSync = async (id) => {
-
+        let returnObject = null;
+        try {
+            const client = await this.getDBClient();
+            const sql = 'SELECT * from provinces WHERE id=$1';
+            const values = [id];
+            const result = await client.query(sql, values);
+            returnObject = result.rows[0];
+        }
+        catch (error){
+            console.logError(error);
+        }
+        return returnObject;
     }
     createAsync = async (entity) => {
-        
+        let rowsAffected = 0;
+        try {
+            const client = new Client(config);
+            await client.connect();
+            const sql = `
+                INSERT INTO provinces
+                    (name, full_name, latitute, longitude, display_order)
+                VALUES
+                    ($1, $2, $3, $4, $5)
+            `;
+            const values = [entity.name, entity.full_name, entity.latitude, entity.longitude, entity.display_order];
+            const result = await client.query(sql, values);
+            rowsAffected = result.rowCount;
+        }
+        catch (error){
+            console.logError(error);
+        }
+        await client.end();
+        return rowsAffected;
     }
     updateAsync = async (entity) => {
-        
+        let rowsAffected = 0;
+        try {
+            const client = new Client(config);
+            await client.connect();
+            const sql = `
+                UPDATE provinces
+                    (name, full_name, latitute, longitude, display_order)
+                VALUES
+                    ($1, $2, $3, $4, $5)
+            `;
+            const values = [entity.name, entity.full_name, entity.latitude, entity.longitude, entity.display_order];
+            const result = await client.query(sql, values);
+            rowsAffected = result.rowCount;
+        }
+        catch (error){
+            console.logError(error);
+        }
+        await client.end();
+        return rowsAffected;
     }
     deleteByIdAsync = async (id) => {
-        
+        let rowsAffected = 0;
+        try {
+            const client = await this.getDBClient();
+            const sql = 'DELETE from provinces WHERE id=$1';
+            const values = [id];
+            const result = await client.query(sql, values);
+            rowsAffected = result.rowCount;
+        }
+        catch (error){
+            console.logError(error);
+        }
+        return rowsAffected;
     }
 }
